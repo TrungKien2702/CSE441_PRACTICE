8b7f9bc21302accbfeeaac74cc79bfb3526311e2
TrungKien2702 <trungkien270220033@gmail.com>
2024-10-04 09:03:00 +0700

    first commit

---
+++ app/src/main/java/com/example/prac1/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.prac1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1;
+    private TextView resultTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button startActivityButton = findViewById(R.id.btnStartActivity);
+        resultTextView = findViewById(R.id.txtResult);
+
+        startActivityButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivityForResult(intent, REQUEST_CODE);
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                String name = data.getStringExtra("name");
+                String gpa = data.getStringExtra("gpa");
+                resultTextView.setText("Họ và tên: " + name + "\nĐiểm GPA: " + gpa);
+            }
+        }
+    }
+}
