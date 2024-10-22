64d16b4c71810bca8e94274c32c9608ae03598f1
TrungKien2702 <trungkien270220033@gmail.com>
2024-10-22 09:20:22 +0700

    Bai11

---
+++ Bai11/app/src/main/java/com/example/bai11/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.bai11;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnopen;
+    EditText edtlink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        edtlink = findViewById(R.id.editTextText);
+        btnopen = findViewById(R.id.button);
+        btnopen.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://" + edtlink.getText().toString()));
+            startActivity(intent);
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
