aea410a421009cd6b66d87e323e52ad17194ffd7
TrungKien2702 <trungkien270220033@gmail.com>
2024-10-25 11:40:23 +0700

    Hoàn thiện bài 22

--- Bai22/app/build.gradle.kts
+++ Bai22/app/build.gradle.kts
@@ -30,7 +30,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
-
+android{
+    useLibrary("org.apache.http.legacy")
+}
 dependencies {
 
     implementation(libs.appcompat)
